44e474ce40f1b388de8c639fbfdad5684319bbeb
Cláudia Carapiá Ferraz <ccarapiaf@gmail.com>
2023-09-21 16:24:04 -0300

    Update Program.cs

--- src/apis-web-services-projeto-reciclaí/apis-web-services-projeto-reciclaí/Program.cs
+++ src/apis-web-services-projeto-reciclaí/apis-web-services-projeto-reciclaí/Program.cs
@@ -1,11 +1,13 @@
 using apis_web_services_projeto_reciclaí.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles); 
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
