dd331adc4b57dda550b7d37f77205b5402761273
Cláudia Carapiá Ferraz <ccarapiaf@gmail.com>
2023-09-20 21:10:40 -0300

    Create UsuariosController.cs

---
+++ src/apis-web-services-projeto-reciclaí/apis-web-services-projeto-reciclaí/Controllers/UsuariosController.cs
@@ -0,0 +1,58 @@
+﻿using apis_web_services_projeto_reciclaí.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace apis_web_services_projeto_reciclaí.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UsuariosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            var model = await _context.Usuarios.ToListAsync();
+            return Ok(model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Usuario model)
+        {
+            _context.Usuarios.Add(model);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetById", new { id = model.Id, model });
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            var model = await _context.Usuarios
+                .FirstOrDefaultAsync(s => s.Id == id);
+
+            if (model == null) return NotFound();
+
+            return Ok(model);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var model = await _context.Usuarios.FindAsync(id);
+
+            if (model == null) return NotFound();
+
+            _context.Usuarios.Remove(model);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
