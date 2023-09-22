805628dda711de098322f80eb68535fafa048f26
Cláudia Carapiá Ferraz <ccarapiaf@gmail.com>
2023-09-21 21:53:32 -0300

    Create UsuarioDto.cs

---
+++ src/apis-web-services-projeto-reciclaí/apis-web-services-projeto-reciclaí/Models/UsuarioDto.cs
@@ -0,0 +1,29 @@
+﻿using apis_web_services_projeto_reciclai.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace apis_web_services_projeto_reciclaí.Models
+{
+    public class UsuarioDto
+    {
+        public int? Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Senha { get; set; }
+
+        [Required]
+        [Display(Name = "Endereço")]
+        public string Endereco { get; set; }
+
+        [Required]
+        public Perfil Perfil { get; set; }
+
+        public TipoLixo TipoLixo { get; set; }
+    }
+}
