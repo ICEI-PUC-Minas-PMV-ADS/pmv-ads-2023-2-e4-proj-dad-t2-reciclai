a2d57775c07d1db79f3a005c6668f72cd75b6358
Cláudia Carapiá Ferraz <ccarapiaf@gmail.com>
2023-09-28 21:40:38 -0300

    Criação Pasta de Testes

---
+++ src/apis-web-services-projeto-reciclaí/apis-web-services-projeto-reciclaí/Test/UsuariosControllerTest.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace apis_web_services_projeto_reciclai.Test
+{
+    [TestClass]
+    public class UsuariosControllerTest
+    {
+
+    }
+}
