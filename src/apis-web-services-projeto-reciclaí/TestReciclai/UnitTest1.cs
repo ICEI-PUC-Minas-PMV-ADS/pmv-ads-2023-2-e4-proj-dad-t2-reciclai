6aaa2a1af7366c021468bdb8a8cd4fad75cf3791
Cláudia Carapiá Ferraz <ccarapiaf@gmail.com>
2023-10-03 10:48:50 -0300

    Criação novo projeto de teste

---
+++ src/apis-web-services-projeto-reciclaí/TestReciclai/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestReciclai
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
