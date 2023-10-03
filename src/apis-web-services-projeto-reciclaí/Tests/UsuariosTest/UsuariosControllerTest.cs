bfaa620212f9afef26062ccb98a8b848e7648eb6
Cláudia Carapiá Ferraz <ccarapiaf@gmail.com>
2023-10-03 08:51:07 -0300

    Alterar nome do teste de usuário

---
+++ src/apis-web-services-projeto-reciclaí/Tests/UsuariosTest/UsuariosControllerTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+using apis_web_services_projeto_reciclai.Models;
+using apis_web_services_projeto_reciclai.Controllers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework.Internal;
+using System;
+using apis_web_services_projeto_reciclaí.Models;
+
+namespace ReciclaiTests.UsuariosTest;
+
+[TestFixture]
+public class UsuariosControllerTest
+{
+
+   
+}
+
