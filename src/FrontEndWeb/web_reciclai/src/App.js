73fb3da940b3597d72a39cfa72e1ff6db170e2a9
keirosu <103314640+keirosu@users.noreply.github.com>
2023-10-15 22:54:25 +0900

    Update App.js

--- src/FrontEndWeb/web_reciclai/src/App.js
+++ src/FrontEndWeb/web_reciclai/src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 import Header from "./components/Header.js";
 
 import Home from "./pages/Home.js";
-import CadastroUsuario from './pages/CadastroUsuario.js'
-
+import CadastroUsuario from "./pages/CadastroUsuario.js";
+import PerfilUsuario from "./pages/PerfilUsuario.js";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<PerfilUsuario/>} />
           <Route path="/cadastro" element={<CadastroUsuario />} />
 
         </Routes>
