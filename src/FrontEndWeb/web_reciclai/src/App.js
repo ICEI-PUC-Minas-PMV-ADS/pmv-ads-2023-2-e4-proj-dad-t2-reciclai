38714e446e48e66f765ca9a2a45f20b5c492507d
Karen Noguti <jkjvida@hotmail.com>
2023-10-16 00:29:51 +0900

    Desenvolvimento Page Home

--- src/FrontEndWeb/web_reciclai/src/App.js
+++ src/FrontEndWeb/web_reciclai/src/App.js
@@ -1,18 +1,18 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import './App.css';
-import Header from "./components/Header.js";
-
 import Home from "./pages/Home.js";
-import CadastroUsuario from "./pages/CadastroUsuario.js";
-import PerfilUsuario from "./pages/PerfilUsuario.js";
+import CadastroUsuario from './pages/CadastroUsuario.js';
+import Header from './components/Header.js';
+
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<PerfilUsuario/>} />
-          <Route path="/cadastro" element={<CadastroUsuario />} />
+          <Route path="/" element={<Home/>} />
+          <Route path="usuarios" element={<CadastroUsuario />} />
 
         </Routes>
       </BrowserRouter>
--- src/FrontEndWeb/web_reciclai/src/pages/Home.js
+++ src/FrontEndWeb/web_reciclai/src/pages/Home.js
@@ -1,9 +1,60 @@
+import { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import Reciclando from "../img/Reciclando.jpg"
+import Reciclando1 from "../img/Reciclando1.jpg"
+import Reciclando2 from "../img/Reciclando2.jpg"
+
+
 function Home() {
-    return (
+   const [index, setIndex] = useState(0);
 
- <h1>Home</h1>
- 
-    );
-  }
+   const handleSelect = (selectedIndex) => {
+      setIndex(selectedIndex);
+   };
 
-  export default Home;
+   return (
+      <Carousel data-bs-theme="dark">
+      <Carousel.Item >
+        <img 
+          className="d-block w-30" 
+          src={Reciclando}
+          alt="Firt slide"
+          height={300}
+          />
+         
+        <Carousel.Caption>
+          <h5>First slide label</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-30"
+          src={Reciclando1}
+          alt="Second slide"
+          height={300}
+          />
+      
+        <Carousel.Caption>
+          <h5>Second slide label</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-30"
+          src={Reciclando2}
+          alt="Third slide"
+          height={300}
+        />
+        <Carousel.Caption>
+          <h5>Third slide label</h5>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+   );
+}
+export default Home;
