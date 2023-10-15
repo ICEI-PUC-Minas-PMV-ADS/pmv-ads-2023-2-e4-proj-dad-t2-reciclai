9cf18c527066b850c087398fbae18222b5d8ac03
Cláudia Carapiá Ferraz <ccarapiaf@gmail.com>
2023-10-14 22:22:43 -0300

    Create Navbar

--- src/FrontEndWeb/web_reciclai/src/App.js
+++ src/FrontEndWeb/web_reciclai/src/App.js
@@ -1,15 +1,22 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import './App.css';
+import Navbar from "./components/Navbar.js";
 
+import Home from "./pages/Home/Home.js";
 import CadastroUsuario from './pages/CadastroUsuario/index.js'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<CadastroUsuario />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="usuarios" element={<CadastroUsuario />} />
+
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/FrontEndWeb/web_reciclai/src/pages/CadastroUsuario/index.js
+++ src/FrontEndWeb/web_reciclai/src/pages/CadastroUsuario/index.js
@@ -1,7 +1,15 @@
-function CadastroUsuario(){
-    return(
-        <h1> Cadastro de Usuário </h1>
-    )
-}
+import { useState } from "react";
+import "./CadastroUsuario.module.css";
+
+
+const CadastroUsuario = ({ user }) => {
+
+
+  return (
+    <div>
+   <h1>Cadastro de Usuario</h1>
+    </div>
+  );
+};
 
 export default CadastroUsuario;
