a4de997fb1edaa4abfe2d305639f7d4f354aa0ff
Cláudia Carapiá Ferraz <ccarapiaf@gmail.com>
2023-10-16 09:36:26 -0300

    Criação componente SelectPerfil.js

---
+++ src/FrontEndWeb/web_reciclai/src/components/SelectPerfil.js
@@ -0,0 +1,26 @@
+import { Box, TextField, MenuItem } from '@mui/material';
+import { useState } from 'react';
+
+export const SelectPerfil = () => {
+
+    const [perfil, setPerfil] = useState('');
+    const handleChange = (e) => {
+       setPerfil(e.target.value) 
+    }
+
+    return(
+        <Box>
+            <TextField 
+            label="Perfil"
+            variant="standard"
+            select
+            value={perfil}
+            onChange={handleChange}
+            fullWidth >
+                <MenuItem value={0}>Solicitante</MenuItem>
+                <MenuItem value={1}>Coletor</MenuItem>
+            </TextField>
+
+        </Box>
+    )
+}
