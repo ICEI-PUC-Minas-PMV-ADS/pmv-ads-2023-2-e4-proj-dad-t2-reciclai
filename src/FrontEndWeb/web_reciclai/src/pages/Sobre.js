0956499af7e1ca556e8e62790d43246766a39817
keirosu <103314640+keirosu@users.noreply.github.com>
2023-10-21 10:48:03 +0900

    Create Sobre.js

---
+++ src/FrontEndWeb/web_reciclai/src/pages/Sobre.js
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap";
+import { useNavigate } from 'react-router-dom';
+import { Link } from "react-router-dom";
+
+
+function Sobre(){
+    return(
+        <Container>
+            blablabla
+        </Container>
+    )
+}
+export default Sobre;
