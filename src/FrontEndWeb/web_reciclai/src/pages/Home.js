9281d8bb1d511db5c98fdba73f8655c60498e77e
Karen Noguti <jkjvida@hotmail.com>
2023-10-22 00:11:46 +0900

    Update Home.js

--- src/FrontEndWeb/web_reciclai/src/pages/Home.js
+++ src/FrontEndWeb/web_reciclai/src/pages/Home.js
@@ -1,17 +1,43 @@
-import { useState } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
+import { useState, useEffect } from 'react';
+import {Carousel, Stack, Container,Button,Form} from 'react-bootstrap';
 import Natureza from "../img/Natureza.jpg";
 import Eletronico from "../img/Eletronico.jpg";
 import People from "../img/people.png";
 import Depoimentos from '../components/Depoimentos'
+import { insertOpiniaoUsuarios } from '../services/OpiniaoUsuario.services';
+import { useNavigate, Link } from 'react-router-dom';
+import styles from './Home.module.css';
 
-function Home() {
-  const [index, setIndex] = useState(0);
-  const handleSelect = (selectedIndex) => {
-    setIndex(selectedIndex);
-  };
+
+const Home = () => {
+  const navigate = useNavigate();
+  const [nome, setNome] = useState('');
+  const [mensagem, setMensagem] = useState('');
+
+  useEffect(() => {
+    async function postUser() {
+      await insertOpiniaoUsuarios().then(item => {
+        if (item) {
+          setNome(item.nome);
+          setMensagem(item.mensagem);
+        }
+      })
+    }
+    postUser();
+  }, []);
+
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    await insertOpiniaoUsuarios({
+      "nome": nome,
+      "mensagem": mensagem,
+    },
+      navigate('/'));
+
+  }
   return (
-    <><Carousel data-bs-theme="dark">
+    <><><Carousel data-bs-theme="dark">
       <Carousel.Item>
         <img
           className="d-block w-100 "
@@ -52,14 +78,37 @@ function Home() {
         <Carousel.Caption
           className="d-block w-100 h-50">
           <h5></h5>
-          <p>
-
-          </p>
+          <p></p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
-      <Depoimentos></Depoimentos></>
+      <Depoimentos></Depoimentos>
+
+    </><Form>
+        <Container>
+          <fieldset>
+            <h5 className={styles.Home}>Deixe Seu Comentário</h5>
+            <form onSubmit={(event) => handleSubmit(event)} action={<Link to="/" />}>
+
+            </form>
+            <Stack spacing={2} direction="row" sx={{ marginBottom: 4 }}>
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="TextInput"></Form.Label>
+              <Form.Control id="nome" placeholder="Nome:" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="TextInput"></Form.Label>
+              <Form.Control id="mensagem" placeholder="Comentário:" />
+            </Form.Group>
+          <Form.Group className="mb-3">
+          </Form.Group>
+          </Stack>
+          <Button type="submit">Enviar</Button>
+        </fieldset>
+      </Container>
+    </Form ></>
   );
 }
+
 export default Home;
 
