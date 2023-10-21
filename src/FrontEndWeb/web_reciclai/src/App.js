859fe1fecf4ab95cf0d6e3fadab682d16f5d5924
keirosu <103314640+keirosu@users.noreply.github.com>
2023-10-21 10:47:54 +0900

    Update App.js

--- src/FrontEndWeb/web_reciclai/src/App.js
+++ src/FrontEndWeb/web_reciclai/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import Home from "./pages/Home.js";
 import CadastroUsuario from './pages/CadastroUsuario.js';
 import PerfilUsuario from  './pages/PerfilUsuario.js';
+import Sobre from  './pages/Sobre.js';
 import Header from './components/Header.js';
-
 
 function App() {
   return (
@@ -13,9 +13,10 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="/sobre" element={<Sobre />} />
           <Route path="/cadastro" element={<CadastroUsuario />} />
           <Route path="/perfil" element={<PerfilUsuario />} />
-
+          
         </Routes>
       </BrowserRouter>
     </div>
