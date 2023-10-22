3fea7ad613149602c2f1034e832a55df933a1208
keirosu <103314640+keirosu@users.noreply.github.com>
2023-10-22 19:16:18 +0900

    Update PerfilUsuario.js

--- src/FrontEndWeb/web_reciclai/src/pages/PerfilUsuario.js
+++ src/FrontEndWeb/web_reciclai/src/pages/PerfilUsuario.js
@@ -1,83 +1,63 @@
-import Button from 'react-bootstrap/Button';
-import { Container, InputLabel } from '@mui/material';
-import Botao from '../components/Button.js';
-import styles from './CadastroUsuario.module.css';
 import React, { useState, useEffect } from 'react';
-import Input from '../components/Input.js';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { Container, Stack } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import { Link } from "react-router-dom";
-
+import { getUsuario } from '../services/Usuarios.services.js';
 
 const PerfilUsuario = () => {
-  async function handleSubmit(event) {
-    event.preventDefault();
-    
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [endereco, setEndereco] = useState('');
+  const [perfil, setPerfil] = useState('');
+  const [tipoLixo, setTipoLixo] = useState('');
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    fetchUsuario();
+  }, []);
+
+  async function fetchUsuario() {
+    try {
+      const res = await getUsuario();
+      setNome(res.nome);
+      setEmail(res.email);
+      setEndereco(res.endereco);
+      setPerfil(res.perfil);
+      setTipoLixo(res.tipoLixo);
+    } catch (error) {
+      console.error('Erro ao buscar informações do usuário:', error);
+    }
   }
-  
 
+  const handleEditar = () => {
+    navigate('/cadastro');
+  };
+
+  const handleExcluir = () => {
+  };
 
   return (
-    <React.Fragment>
     <Container>
-      <h2 className={styles.form}>Perfil de Usuário</h2>
-      <form onSubmit={(event) => handleSubmit(event)} action={<Link to="/" />}>
-
-      
-      <div >
-        <Input
-            disabled
-            id="nomeUsuario"
-            label="Nome Usuário" 
-          />
-        </div>
-      <div >
-        <Input
-            disabled
-            id="emailUsuario"
-            label="Email" 
-          />
-      </div>
-      <div >
-        <Input
-            disabled
-            id="enderecoUsuario"
-            label="Endereço" 
-          />
-      </div>
-
-      <div >
-        <Input
-            disabled
-            id="perfilUsuario"
-            label="Perfil" 
-          />
-      </div>
-
-      <div >
-        <Input
-            disabled
-            id="tipoLixo"
-            label="Tipo de Lixo" 
-          />
-      </div>
-      
-      <div> . </div>
-      
-      <div >
-      <Botao type="submit" className={styles.botao} >
-          Editar        
-        </Botao>
-        </div>
-        <div> . </div>
-      <div >
-      <Botao className={styles.botao} >
-          Excluir        
-        </Botao>
-      </div>
-      </form>
+      <Card body outline color="success" className="mx-auto my-5" style={{ width: '50rem' }}>
+        <Card.Body>
+          <Card.Title>Perfil do Usuário</Card.Title>
+          <Card.Text>Nome: {nome}</Card.Text>
+          <Card.Text>Email: {email}</Card.Text>
+          <Card.Text>Endereço: {endereco}</Card.Text>
+          <Card.Text>Perfil: {perfil}</Card.Text>
+          <Card.Text>Tipo de Lixo: {tipoLixo}</Card.Text>
+          <Stack spacing={2} direction="row" sx={{ marginBottom: 4 }}>
+            <Button variant="primary" onClick={handleEditar}>
+              Editar
+            </Button>
+            <Button variant="outline-danger" onClick={handleExcluir}>
+              Excluir
+            </Button>
+          </Stack>
+        </Card.Body>
+      </Card>
     </Container>
-    </React.Fragment>
   );
 };
 
