1bce64ceee6b774ff8ebba6056e4df00661af808
Karen Noguti <jkjvida@hotmail.com>
2023-10-27 10:10:47 +0900

    Update Depoimentos.js

--- src/FrontEndWeb/web_reciclai/src/components/Depoimentos.js
+++ src/FrontEndWeb/web_reciclai/src/components/Depoimentos.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Card, Col, Row } from 'react-bootstrap';
-import { getOpiniaoUsuarios } from '../services/OpiniaoUsuario.services'; 
+import { getOpiniaoUsuarios } from '../services/OpiniaoUsuario.services';
 
 function Depoimentos() {
   const [depoimentos, setDepoimentos] = useState([]);
-  
+
   useEffect(() => {
     async function fetchDepoimentos() {
       const data = await getOpiniaoUsuarios();
@@ -15,16 +15,28 @@ function Depoimentos() {
     fetchDepoimentos();
   }, []);
 
+  const limitarCaracteres = (texto, limite) => {
+    if (texto.length <= limite) {
+      return texto;
+    } else {
+      return texto.slice(0, limite) + '...'; 
+    }
+  };
+  
   return (
     <Container>
-      <h8>Depoimentos: </h8>
+      <Container className="containerHome">
+      <h6 >Depoimentos:</h6>
+      </Container>
       <Row xs={1} md={3} className="g-3">
         {depoimentos.map((depoimento, idx) => (
           <Col key={idx}>
             <Card body outline color="success" className="mx-auto my-5">
-              <Card.Body>
+              <Card.Body style={{ height: '150px' }}>
                 <Card.Title>{depoimento.nome}</Card.Title>
-                <Card.Text>{depoimento.mensagem}</Card.Text>
+                <Card.Text style={{ whiteSpace: 'pre-line' }}>
+                  {limitarCaracteres(depoimento.mensagem, 150)}
+                </Card.Text>
               </Card.Body>
             </Card>
           </Col>
