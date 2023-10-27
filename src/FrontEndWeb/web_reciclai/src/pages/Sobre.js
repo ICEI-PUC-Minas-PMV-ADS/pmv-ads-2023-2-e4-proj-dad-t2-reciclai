65d49bed56e9e28bc23b39be838760a37620a38b
keirosu <103314640+keirosu@users.noreply.github.com>
2023-10-28 08:35:10 +0900

    Update Sobre.js

--- src/FrontEndWeb/web_reciclai/src/pages/Sobre.js
+++ src/FrontEndWeb/web_reciclai/src/pages/Sobre.js
@@ -1,10 +1,17 @@
 import { Container } from "react-bootstrap";
+import Natureza from "../img/Natureza.jpg";
+
+
 
 function Sobre(){
     return(
         <Container>
-           informaçoes do projeto
+
+           informaçoes do projeto           
+           
         </Container>
+
+        
     )
 }
 export default Sobre;
