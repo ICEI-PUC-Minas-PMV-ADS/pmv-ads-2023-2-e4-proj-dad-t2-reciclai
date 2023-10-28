7bf0ba60b0acd5faf503aa52200af8990f124e57
Cláudia Carapiá Ferraz <ccarapiaf@gmail.com>
2023-10-28 15:38:04 -0300

    Criação Modal

--- src/FrontEndWeb/web_reciclai/src/pages/HomeAposLogin.js
+++ src/FrontEndWeb/web_reciclai/src/pages/HomeAposLogin.js
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Modal, Button } from 'react-bootstrap';
 import styles from './styles/HomeAposLogin.module.css';
 import { getTodosPedidos } from '../services/Pedidos.services';
-import { Table, Paper, TableBody, TableCell, TableContainer, TableHead, TableRow, Stack } from '@mui/material';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import IconButton from '@mui/material/IconButton';
-import LogoutIcon from '@mui/icons-material/Logout';
+import { Table, Paper, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { useUser } from '../contexts/UserContext';
 
@@ -15,13 +12,16 @@ const HomeAposLogin = () => {
   const navigate = useNavigate();
   const { userId } = useUser();
   const [data, setData] = useState([]);
-  
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   useEffect(() => {
     async function fetchPedidos() {
       const data = await getTodosPedidos();
       if (data) {
         let PedidosUsuario = [];
-        PedidosUsuario = data.filter((pedido)=> pedido.idSolicitante === userId);
+        PedidosUsuario = data.filter((pedido) => pedido.idSolicitante === userId);
         setData(PedidosUsuario);
         console.log(data);
       }
@@ -29,16 +29,11 @@ const HomeAposLogin = () => {
     fetchPedidos();
   }, []);
 
-  async function redirecionar (){
-    navigate('/perfil')
-  }
-
- 
 
   return (
     <Container>
 
-    <div>
+      <div>
         <h5 className={styles.titulo}>Histórico de pedidos:</h5>
         <TableContainer component={Paper} className={styles.table}>
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -47,7 +42,7 @@ const HomeAposLogin = () => {
                 <TableCell align="left">Id</TableCell>
                 <TableCell align="left">Nome</TableCell>
                 <TableCell align="left">Data Coleta</TableCell>
-                <TableCell align="left">Tipo de Lixo</TableCell>
+                <TableCell align="left">Operação</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -59,12 +54,37 @@ const HomeAposLogin = () => {
                   <TableCell align="left">{pedido.id}</TableCell>
                   <TableCell align="left">{pedido.nomeSolicitante}</TableCell>
                   <TableCell align="left">{pedido.dataColeta}</TableCell>
-                  <TableCell align="left">{pedido.tipoLixo}</TableCell>
+                  <TableCell align="left">
+                    <Button onClick={handleShow}>Visualizar</Button>
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         </TableContainer>
+
+        <Modal show={show} onHide={handleClose} >
+          <Modal.Header closeButton>
+            <Modal.Title>Pedido: </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Nome:</p>
+            <p>Data da Coleta: </p>
+            <p>Tipo de Lixo:</p>
+            <p>Quantidade de Lixo:</p>
+            <p>Descrição:</p>
+            <p>Endereço: </p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Fechar
+            </Button>
+            <Button variant="danger" onClick={handleClose}>
+              Cancelar Pedido
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
       </div>
     </Container>
   );
