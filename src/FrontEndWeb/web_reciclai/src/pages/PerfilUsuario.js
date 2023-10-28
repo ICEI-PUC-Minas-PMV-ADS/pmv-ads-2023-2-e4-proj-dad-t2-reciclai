d74dfd22b0c79fb780e92d86141026d53e1112fa
keirosu <103314640+keirosu@users.noreply.github.com>
2023-10-28 09:53:22 +0900

    Update PerfilUsuario.js

--- src/FrontEndWeb/web_reciclai/src/pages/PerfilUsuario.js
+++ src/FrontEndWeb/web_reciclai/src/pages/PerfilUsuario.js
@@ -1,63 +1,67 @@
-import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import { Container, Stack } from '@mui/material';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { getUsuario } from '../services/Usuarios.services.js';
+import { useUser } from '../contexts/UserContext.js'
+
 
 const PerfilUsuario = () => {
+  const {userId} = useUser();
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
   const [endereco, setEndereco] = useState('');
-  const [perfil, setPerfil] = useState('');
-  const [tipoLixo, setTipoLixo] = useState('');
+  const [perfil, setPerfil] = useState();
+  const [tipoLixo, setTipoLixo] = useState();
   const navigate = useNavigate();
 
   useEffect(() => {
-    fetchUsuario();
-  }, []);
+    fetchUsuario()
+  }, [])
+
 
   async function fetchUsuario() {
-    try {
-      const res = await getUsuario();
-      setNome(res.nome);
-      setEmail(res.email);
-      setEndereco(res.endereco);
-      setPerfil(res.perfil);
-      setTipoLixo(res.tipoLixo);
-    } catch (error) {
-      console.error('Erro ao buscar informações do usuário:', error);
-    }
-  }
+    //const id = 1; //ter que passar usuarioId apos login, para testes usar usuarioId hardcoded (criar usuario)
+    const res = await getUsuario(userId) 
+    console.log(res)
+    setNome(res.nome);
+    setEmail(res.email);
+    setSenha(res.senha);
+    setEndereco(res.endereco);
+    setPerfil(res.perfil);
+    setTipoLixo(res.tipoLixo);
 
-  const handleEditar = () => {
-    navigate('/cadastro');
-  };
-
-  const handleExcluir = () => {
   };
 
   return (
-    <Container>
-      <Card body outline color="success" className="mx-auto my-5" style={{ width: '50rem' }}>
-        <Card.Body>
-          <Card.Title>Perfil do Usuário</Card.Title>
-          <Card.Text>Nome: {nome}</Card.Text>
-          <Card.Text>Email: {email}</Card.Text>
-          <Card.Text>Endereço: {endereco}</Card.Text>
-          <Card.Text>Perfil: {perfil}</Card.Text>
-          <Card.Text>Tipo de Lixo: {tipoLixo}</Card.Text>
-          <Stack spacing={2} direction="row" sx={{ marginBottom: 4 }}>
-            <Button variant="primary" onClick={handleEditar}>
-              Editar
-            </Button>
-            <Button variant="outline-danger" onClick={handleExcluir}>
-              Excluir
-            </Button>
-          </Stack>
-        </Card.Body>
-      </Card>
-    </Container>
+    <React.Fragment>
+      <Container>
+        <Card body outline color="success" className="mx-auto my-5" style={{ width: '50rem' }}>
+          <Card.Body>
+            <Card.Title>{nome}</Card.Title>
+            <Card.Text>
+              Email: {email}
+            </Card.Text>
+            <Card.Text>
+              Endereço: {endereco}
+            </Card.Text>
+            <Card.Text>
+              Perfil: {perfil}
+            </Card.Text>
+            <Card.Text>
+              Tipo de Lixo: {tipoLixo}
+            </Card.Text>
+            <Stack spacing={2} direction="row" sx={{ marginBottom: 4 }}>
+            <Button variant="primary">Editar</Button>
+            <Button variant="outline-danger">Excluir</Button>
+            </Stack>
+          </Card.Body>
+        </Card>
+      </Container>
+    </React.Fragment>
   );
 };
 
