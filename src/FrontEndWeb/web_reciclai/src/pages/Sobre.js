3657ac318450361a107f6c24e7c62dd55b3d6ce0
keirosu <103314640+keirosu@users.noreply.github.com>
2023-10-29 23:23:55 +0900

    Atualização de sobre

--- src/FrontEndWeb/web_reciclai/src/pages/Sobre.js
+++ src/FrontEndWeb/web_reciclai/src/pages/Sobre.js
@@ -1,19 +1,28 @@
 import { Container } from '@mui/material';
 import styles from './styles/Sobre.module.css';
-import Reciclando from "../img/Reciclando.jpg";
+import seta from "../img/seta.gif";
+import lata from "../img/lata.gif";
+import setadupla from "../img/setadupla.gif";
+import retorno from "../img/retorno.gif";
+import Natureza from "../img/Natureza.jpg";
+import Image from 'react-bootstrap/Image';
+
 
 function Sobre(){
     return(
         <Container className={styles.Container}>
             <h1 className={styles.titulo} >Reciclaí</h1>
-            <img src={Reciclando} alt="imagem de reciclados" />
-            <p>
+            <img src={seta} alt="seta.gif" />
+            <img src={lata}  />
+           
+            <h5>
                 Um dos desafios que a tecnologia enfrenta nos últimos 20 anos é a absolência programada. Com o obetivo de fomentar o consumo e
             maximizar os lucros, os componentes eletrônicos possuem um ciclo de vida definido e pouco é feito pelas indústrias e fabricantes 
             para diminuir o problema do lixo gerado. Muitos dos materiais eletrônicos e periféricos podem ser reciclados, sendo o descarte apropriado e a coleta, o principal entrave
             para a reutilização desses componentes. Nosso projeto, o Reciclaí, entra nesse contexto como uma alternativa sustentável. Visando conectar 
             catadores, empresas e consumidores para destinar corretamente o lixo eletrônico, diminuindo o impacto ambiental.
-            </p>
+            </h5>
+            <Image src={Natureza}roundedCircle/>
            
         </Container>
     )
