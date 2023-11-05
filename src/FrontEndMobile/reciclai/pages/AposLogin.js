e734ff6a8407b286788f18d319acaed807d1f3e9
Cláudia Carapiá Ferraz <ccarapiaf@gmail.com>
2023-11-05 15:14:59 -0300

    Atualização AposLogin.js

--- src/FrontEndMobile/reciclai/pages/AposLogin.js
+++ src/FrontEndMobile/reciclai/pages/AposLogin.js
@@ -1,18 +1,77 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, FlatList, ScrollView } from 'react-native';
+import { Headline, List } from 'react-native-paper';
+import { useIsFocused } from '@react-navigation/native';
+
+import Card from '../components/Card';
+import { getTodosPedidos } from '../services/Pedidos.services';
+import Container from '../components/Container';
+import Body from '../components/Body';
 
 const AposLogin = () => {
 
+  const isFocused = useIsFocused();
+  const [pedidos, setPedidos] = useState([]);
+
+  useEffect(() => {
+    getTodosPedidos().then((data) => {
+
+      setPedidos(data);
+    })
+  }, [isFocused]);
+
+
+  const ItemView = ({ item }) => {
+    console.log(item);
+    return (
+      <List.Item
+        title={item.nomeSolicitante}
+      />
+    )
+  }
+
   return (
+    <Container>
+      <Body>
 
-    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}> 
+      <View style={styles.headline}>
+        <Headline style={styles.headline2}>Olá,</Headline>
+      </View>
 
-      <Text> PUC Minas </Text>
-    
-    </View>
+      
+        <ScrollView>
 
+          <Text style={styles.titulo}>Meus Pedidos:</Text>
+          <Card>
+            <FlatList
+              data={pedidos}
+              keyExtractor={item => item.id}
+              renderItem={ItemView}
+            />
+          </Card>
+
+        </ScrollView>
+      </Body>
+    </Container>
   );
 
 }
 
+const styles = StyleSheet.create({
+  titulo: {
+    fontSize: 25,
+    color: '#FFFFFF',
+    marginLeft: 10,
+  }, 
+  headline:{
+    marginTop: 50,
+    marginBottom: 30,
+    marginLeft: 10,
+  },
+  headline2: {
+    color: '#FFFFFF',
+  }
+});
+
 export default AposLogin;
+
