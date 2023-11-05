1eeb7257fba0346d5ae7088bad75347adb52a1ea
Cláudia Carapiá Ferraz <ccarapiaf@gmail.com>
2023-11-05 15:14:43 -0300

    Criação componentes Body, Card, Container

---
+++ src/FrontEndMobile/reciclai/components/Card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Text,StyleSheet, View} from 'react-native';
+
+const Card = ({children, text}) =>{
+  return <View style={styles.containerCard}>{children}
+  <Text style={styles.texto}>{text}</Text>
+    </View>
+};
+
+const styles = StyleSheet.create({
+ containerCard:{ 
+    width:"100%",
+    borderRadius: 10,
+    backgroundColor:"#EDEBEB",
+    padding:10,
+    marginVertical:8
+
+  },
+
+});
+
+export default Card;
