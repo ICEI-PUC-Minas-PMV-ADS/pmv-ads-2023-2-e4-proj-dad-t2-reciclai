b0791e3ef4d29fa997e40ab2dff8a8e9b86f35f9
keirosu <103314640+keirosu@users.noreply.github.com>
2023-11-08 09:49:19 +0900

    Cadastro Usuário

--- src/FrontEndMobile/reciclai/pages/Home.js
+++ src/FrontEndMobile/reciclai/pages/Home.js
@@ -4,17 +4,21 @@ import { BottomNavigation, Text } from 'react-native-paper';
 import Pesquisa from './Pesquisa'; 
 import AposLogin from './AposLogin'; 
 import VerPedido from './VerPedido';
+import CadastroUsuario from './CadastroUsuario';
 
 const Home = () => {
   const [index, setIndex] = React.useState(0);
   const [routes] = React.useState([
     { key: 'pesquisa', title: 'Pesquisa', focusedIcon: 'file-search-outline'},
     { key: 'aposLogin', title: 'AposLogin', focusedIcon: 'album' },
+    { key: 'cadastroUsuario', title: 'CadastroUsuario', focusedIcon: 'album' },
   ]);
 
   const renderScene = BottomNavigation.SceneMap({
     pesquisa: Pesquisa,
-    aposLogin: AposLogin,   
+    aposLogin: AposLogin,
+    cadastroUsuario: CadastroUsuario,
+       
   });
 
   return (
